392beca4e9e0a4384f2884f24a8ad5dfbe2cab6b
THEVENARD Julien <thevenard.julien@gmail.com>
2023-09-23 21:39:56 +0200

    Routes : Home, 404

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import P404 from "./pages/P404";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<P404 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/P404.jsx
@@ -0,0 +1,5 @@
+const P404 = () => {
+  return <div>Impossible de trouver la page... (404)</div>;
+};
+
+export default P404;
