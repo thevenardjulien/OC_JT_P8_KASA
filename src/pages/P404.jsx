5af792b1e790ba6942cac73cf75da35bd8cd2390
THEVENARD Julien <thevenard.julien@gmail.com>
2023-09-25 07:27:26 +0200

    Design P404

--- src/pages/P404.jsx
+++ src/pages/P404.jsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom";
+
 const P404 = () => {
   return (
-    <div className="container-p404">Impossible de trouver la page... (404)</div>
+    <div className="container-p404">
+      <h1 className="container-p404__title">404</h1>
+      <p className="container-p404__text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link className="container-p404__link" to="/">
+        <p>Retourner sur la page d’accueil</p>
+      </Link>
+    </div>
   );
 };
 
