a87ad87d0601f317431b64e18c13fb83d7589064
THEVENARD Julien <thevenard.julien@gmail.com>
2023-09-25 15:51:41 +0200

    Collapse CSS

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,8 +1,38 @@
-const Collapse = () => {
+import { useState } from "react";
+
+const Collapse = ({ title, txt }) => {
+  const [display, setDisplay] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+
+  function handleClick() {
+    setDisplay(!display);
+    setIsActive(!isActive);
+  }
   return (
-    <div className="collapse">
-      <h2 className="collapse__title">Collapse title</h2>
-      <i className="collapse__chevron fa-solid fa-chevron-up"></i>
+    <div className="collapse-container">
+      <div className="collapse">
+        <h2 className="collapse__title">{title}</h2>
+        <i
+          className={
+            isActive
+              ? "collapse__chevron collapse__chevron--active fa-solid fa-chevron-up"
+              : "collapse__chevron fa-solid fa-chevron-up"
+          }
+          onClick={handleClick}
+        ></i>
+      </div>
+      {display && (
+        <div
+          className={
+            isActive
+              ? "collapseContent collapseContent--active"
+              : "collapseContent"
+          }
+        >
+          {" "}
+          {txt}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -1,12 +1,21 @@
 import Banner from "../components/Banner";
 import BannerImg from "../assets/img/bannerabout.png";
 import Collapse from "../components/Collapse";
+import AproposContent from "../assets/json/aproposcontent.json";
+
+const aboutCollapses = AproposContent.map((content, index) => {
+  return (
+    <div key={index}>
+      <Collapse title={content.title} txt={content.txt} />
+    </div>
+  );
+});
 
 const Apropos = () => {
   return (
     <div>
-      <Banner src={BannerImg} txt="" />
-      <Collapse />
+      <Banner src={BannerImg} />
+      {aboutCollapses}
     </div>
   );
 };
