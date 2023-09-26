3a762e16c9a96da0abd68c2ebfa8abc445bf2915
THEVENARD Julien <thevenard.julien@gmail.com>
2023-09-26 08:54:30 +0200

    Add Rating

--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+
 const Rate = ({ rating }) => {
-  console.log(rating);
-  return <div>{rating}</div>;
+  const [rate, setRate] = useState(rating);
+  const star = <i class="fa-solid fa-star"></i>;
+  const emptyStar = <i class="fa-regular fa-star"></i>;
+  const stars = [];
+
+  for (let i = 0; i < rate; i++) {
+    stars.push(star);
+  }
+
+  for (let i = rate; i < 5; i++) {
+    stars.push(emptyStar);
+  }
+
+  return <div>{stars}</div>;
 };
 
 export default Rate;
